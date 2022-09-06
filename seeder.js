f08cf3726d0e0666bd27c1d3ffc04f8241a3bd8e
Mikias <mikias.worash.a@gmail.com>
2022-09-06 20:42:04 +0300

    Course Seeder

--- seeder.js
+++ seeder.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -10,15 +11,21 @@ dotenv.config({ path: './config/config.env' });
 // connect to db
 mongoose.connect(process.env.MONGO_URI);
 
-// Read json files from bootcamps.json
-const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
-);
-
-// Insert data into the database
+// Import data into the db
 const importData = async () => {
+  // Read data(json files) from bootcamps.json
+  const bootcamps = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+  );
+
+  const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+  );
+
+  // Insert the data into the db
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data imported to database...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -30,6 +37,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data destroyed from database...'.red.inverse);
     process.exit();
   } catch (err) {
