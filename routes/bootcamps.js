9f28870604c67303c34a45301efdebc783ae5aa8
Mikias <mikias.worash.a@gmail.com>
2022-09-12 20:45:39 +0300

    Model Router

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,7 +9,13 @@ const {
   getBootcampsInRadius,
 } = require('../controllers/bootcamps');
 
+// Include other resource routers
+const courseRouter = require('./courses');
+
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/').get(getBootcamps).post(createBootcamp);
 
