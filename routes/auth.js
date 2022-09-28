bcee965996a99c17f7993cf392f01185545ca607
Mikias <mikias.worash.a@gmail.com>
2022-09-28 15:01:13 +0300

    User Login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { register } = require('../controllers/auth');
+const { register, login } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.route('/register').post(register);
+router.route('/login').post(login);
 
 module.exports = router;
