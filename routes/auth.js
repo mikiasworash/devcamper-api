d4227bc50dbc022aa748981c60e5fc334787a5e1
Mikias <mikias.worash.a@gmail.com>
2022-09-30 12:30:55 +0300

    Auth Protect Middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
-const { register, login } = require('../controllers/auth');
+const { register, login, getMe } = require('../controllers/auth');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/me').get(protect, getMe);
 
 module.exports = router;
