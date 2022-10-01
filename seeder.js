41aa2ca327fcfd6bfae2c77c3202a042234a27f0
Mikias <mikias.worash.a@gmail.com>
2022-10-01 13:00:24 +0300

    Bootcamp-User Relationship

--- seeder.js
+++ seeder.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
-const Bootcamp = require('./models/Bootcamp');
-const Course = require('./models/Course');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const colors = require('colors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
+const User = require('./models/User');
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI);
@@ -22,10 +25,15 @@ const importData = async () => {
     fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
   );
 
+  const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+  );
+
   // Insert the data into the db
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log('Data imported to database...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -38,6 +46,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log('Data destroyed from database...'.red.inverse);
     process.exit();
   } catch (err) {
