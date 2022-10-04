887ad03ef78455d98c48164eb1593923d75fdc77
Mikias <mikias.worash.a@gmail.com>
2022-10-04 14:10:40 +0300

    Update User Details

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
@@ -14,6 +16,8 @@ const router = express.Router();
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/me').get(protect, getMe);
+router.route('/updatedetails').put(protect, updateDetails);
+router.route('/updatepassword').put(protect, updatePassword);
 router.route('/forgotpassword').post(forgotPassword);
 router.route('/resetpassword/:resettoken').put(resetPassword);
 
